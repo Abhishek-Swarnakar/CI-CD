4b0bb34efc124c997b55c89490102a4d4521bbfb
Abhishek-Swarnakar <abhishekswarnakar2000iem@gmail.com>
2024-10-16 17:02:35 +0530

    FINAL-TESTING

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,4 +4,4 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull abhishekswarnakar/image:latest
 # Run the Docker image as a container
-docker run -d -p 5000:5000  abhishekswarnakar/image:latest
+docker run -d -p 6000:6000  abhishekswarnakar/image:latest
