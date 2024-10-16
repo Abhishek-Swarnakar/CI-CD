cf074544e76d81c45a86dac3cd3826241d0bf36c
Abhishek-Swarnakar <abhishekswarnakar2000iem@gmail.com>
2024-10-16 16:58:35 +0530

    TESTING

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -4,5 +4,5 @@ set -e
 # Stop the running container (if any)
 containerid=$(docker ps -q)  # Use -q to get only the container IDs
 if [ -n "$containerid" ]; then
-  docker rm -f $containerid
+  docker stop $containerid
 fi
