88998bd860bb5c64c019b932f01626e9a1e66f1f
Abhishek-Swarnakar <abhishekswarnakar2000iem@gmail.com>
2024-10-16 16:16:09 +0530

    TESTING

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Install Docker if it's not already installed
-if ! [ -x "$(command -v docker)" ]; then
-  echo "Error: Docker is not installed." >&2
+if ! [ -x "`command -v docker`" ]; then
+  echo 'Error: Docker is not installed.' >&2
   exit 1
 fi
 
 # Stop the running container (if any)
-container_id=$(docker ps -q)
+container_id=`docker ps -q`
 if [ -n "$container_id" ]; then
   docker rm -f $container_id
 fi
