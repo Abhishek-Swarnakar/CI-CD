fa938c0c4d7f925e4b02d916cd53d59a28befe56
Abhishek-Swarnakar <abhishekswarnakar2000iem@gmail.com>
2024-10-18 13:46:01 +0530

    SUCCESSULL!!

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Abhishek!'
+    return 'Hello, I am Abhishek. This is my first CI-CD project!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
